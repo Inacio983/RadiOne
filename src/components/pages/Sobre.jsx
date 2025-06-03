e81a57a0d01465a41e6615fb2f312f03380c485f
unknown <inaciobellaguarda@gmail.com>
2025-06-03 09:45:44 -0300

    radio2

--- src/components/pages/Sobre.jsx
+++ src/components/pages/Sobre.jsx
@@ -8,6 +8,26 @@ export const Sobre = () =>{
                     <h1>Sobre o RadiOne</h1>
                 </div>
             </div>
+
+            <div className="setaResp">
+
+                <div className="esq">
+                    <div className="expl">Mostrar <br/>Display</div>
+                    <div className="expl">Reset</div>
+                </div>
+                
+                <div className="radioRes">
+                    <img src="heltec.svg"></img>
+                </div>
+                
+                <div className="dir">
+                    <div className="expl">Nome da Rede</div>
+                    <div className="expl">Endereço IP</div>
+                    <div className="expl">Número de Salas</div>
+                    <div className="expl">Histórico</div>
+                </div>
+
+            </div>
         </div>
     </>)
 }
