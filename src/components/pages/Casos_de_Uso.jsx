65bef49c1de90e6f1984ed298a9f6b75a6a4c31e
unknown <inaciobellaguarda@gmail.com>
2025-06-20 16:02:49 -0300

    resp4

--- src/components/pages/Casos_de_Uso.jsx
+++ src/components/pages/Casos_de_Uso.jsx
@@ -8,6 +8,12 @@ export const Casos_de_Uso = () =>{
                     <h1>Casos de Uso</h1>
                 </div>
             </div>
+
+            <div className="pcontainer" >
+                <p>Em um mundo cada vez mais conectado, é fácil esquecer que a nossa dependência de redes elétricas e internet pode ser uma vulnerabilidade em momentos críticos. É aí que o RadiOne se revela uma ferramenta indispensável, um elo vital quando todas as outras formas de comunicação falham.</p>
+    
+                <p><br/>Em cenários de desastres naturais a infraestrutura de comunicação pode ser a primeira a ser afetada. Torres de celular caem, linhas telefônicas são danificadas e o acesso à internet é interrompido. Nesses momentos, o rádio é o seu canal direto para o mundo exterior. Ele permite que você receba alertas, instruções de segurança, atualizações sobre a situação além de permitir que você se envie  mensagens informando sua situação.</p>
+            </div>
         </div>
     </>)
 }
